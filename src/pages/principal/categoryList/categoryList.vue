2be404a452bd12698586bcce0f480c060510bf6d
ooobsidian <obsidian_lrx@aliyun.com>
2019-01-16 13:37:03 +0800

    feat(principal): 完成业务员可开通中期报告和结题报告功能的接口

--- src/pages/principal/categoryList/categoryList.vue
+++ src/pages/principal/categoryList/categoryList.vue
@@ -25,18 +25,93 @@
       <br>
       <p>是否上会：{{(data1[index].isExistMeetingReview===1?'是':'否')}}</p>
       <br>
-      <p>是否提交中期报告：{{(data1[index].interimReport.isReportActivated===true)?'是':'否'}}</p>
+      <p>是否可提交中期报告：{{(data1[index].interimReport.isReportActivated===true)?'是':'否 '}}<a
+        v-if="!data1[index].interimReport.isReportActivated" @click="openInterimReport(index)">点击开通</a></p>
+      <br v-if="data1[index].interimReport.isReportActivated">
+      <p v-if="data1[index].interimReport.isReportActivated">中期报告开始时间：{{data1[index].interimReport.startTime}}</p>
+      <br v-if="data1[index].interimReport.isReportActivated">
+      <p v-if="data1[index].interimReport.isReportActivated">中期报告截止时间：{{data1[index].interimReport.deadline}}</p>
       <br>
-      <p>中期报告开始时间：{{(data1[index].interimReport.startTime===null)?'无':data1[index].interimReport.startTime}}</p>
-      <br>
-      <p>中期报告截止时间：{{(data1[index].interimReport.deadline===null)?'无':data1[index].interimReport.deadline}}</p>
-      <br>
-      <p>是否提交结题报告：{{(data1[index].concludingReport.isReportActivated===true)?'是':'否'}}</p>
-      <br>
-      <p>结题报告开始时间：{{(data1[index].concludingReport.startTime===null)?'无':data1[index].concludingReport.startTime}}</p>
-      <br>
-      <p>结题报告截止时间：{{(data1[index].concludingReport.deadline===null)?'无':data1[index].concludingReport.deadline}}</p>
-      <br>
+      <p>是否可提交结题报告：{{(data1[index].concludingReport.isReportActivated===true)?'是':'否 '}}<a
+        v-if="!data1[index].concludingReport.isReportActivated" @click="openConcludingReport(index)">点击开通</a></p>
+      <br v-if="data1[index].concludingReport.isReportActivated">
+      <p v-if="data1[index].concludingReport.isReportActivated">结题报告开始时间：{{data1[index].concludingReport.startTime}}</p>
+      <br v-if="data1[index].concludingReport.isReportActivated">
+      <p v-if="data1[index].concludingReport.isReportActivated">结题报告截止时间：{{data1[index].concludingReport.deadline}}</p>
+    </Modal>
+    <Modal v-model="modal3" title="开通提交中期报告" @on-ok="ok1(index)" @on-cancel="cancel1(index)">
+      <Form ref="interimReport" :model="interimReport" :rules="interimReportRule">
+        <FormItem label="中期报告开始时间" prop="startTime">
+          <DatePicker type="datetime" :value="interimReport.startTime" v-model="interimReport.startTime"
+                      format="yyyy年MM月dd日 HH:mm" style="width: 300px"
+                      placeholder="选择开始时间"></DatePicker>
+        </FormItem>
+        <FormItem label="中期报告截止时间" prop="startTime">
+          <DatePicker type="datetime" :value="interimReport.endTime" v-model="interimReport.endTime"
+                      format="yyyy年MM月dd日 HH:mm" style="width: 300px"
+                      placeholder="选择截止时间"></DatePicker>
+        </FormItem>
+        <FormItem label="上传模板" prop="uploadAddress">
+          <Upload
+            type="drag"
+            :headers="uploadHeaders"
+            :on-success="uploadSuccess1"
+            ref="upload"
+            action="http://129.204.71.113:8888/api/file/upload">
+            <div style="padding: 20px 0">
+              <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+              <p>点击或将文件拖拽到这里上传</p>
+            </div>
+          </Upload>
+        </FormItem>
+      </Form>
+      <div slot="footer">
+        <Button type="text" @click.native="cancel1(index)"
+                style="">
+          取消
+        </Button>
+        <Button type="primary" @click.native="ok1(index)"
+                style="">
+          确定
+        </Button>
+      </div>
+    </Modal>
+    <Modal v-model="modal4" title="开通提交结题报告" @on-ok="ok2(index)" @on-cancel="cancel2(index)">
+      <Form ref="concludingReport" :model="concludingReport" :rules="concludingReportRule">
+        <FormItem label="中期报告开始时间" prop="startTime">
+          <DatePicker type="datetime" :value="concludingReport.startTime" v-model="concludingReport.startTime"
+                      format="yyyy年MM月dd日 HH:mm" style="width: 300px"
+                      placeholder="选择开始时间"></DatePicker>
+        </FormItem>
+        <FormItem label="中期报告截止时间" prop="startTime">
+          <DatePicker type="datetime" :value="concludingReport.endTime" v-model="concludingReport.endTime"
+                      format="yyyy年MM月dd日 HH:mm" style="width: 300px"
+                      placeholder="选择截止时间"></DatePicker>
+        </FormItem>
+        <FormItem label="上传模板" prop="uploadAddress">
+          <Upload
+            type="drag"
+            :headers="uploadHeaders"
+            :on-success="uploadSuccess2"
+            ref="upload"
+            action="http://129.204.71.113:8888/api/file/upload">
+            <div style="padding: 20px 0">
+              <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+              <p>点击或将文件拖拽到这里上传</p>
+            </div>
+          </Upload>
+        </FormItem>
+      </Form>
+      <div slot="footer">
+        <Button type="text" @click.native="cancel2(index)"
+                style="">
+          取消
+        </Button>
+        <Button type="primary" @click.native="ok2(index)"
+                style="">
+          确定
+        </Button>
+      </div>
     </Modal>
   </div>
 </template>
@@ -58,8 +133,45 @@
         modal1_delay: false,
         loading: false,
         modal1: false,
+        modal3: false,
+        modal4: false,
         infoTitle: null,
         index: 0,
+        interimReport: {
+          startTime: '',
+          endTime: '',
+          uploadAddress: ''
+        },
+        concludingReport: {
+          startTime: '',
+          endTime: '',
+          uploadAddress: ''
+        },
+        interimReportRule: {
+          startTime: [
+            {required: true, type: 'date', message: '请选择开始时间', trigger: 'blur'}
+          ],
+          endTime: [
+            {required: true, type: 'date', message: '请选择截止时间', trigger: 'blur'}
+          ],
+          uploadAddress: [
+            {required: true, message: '请上传模板', trigger: 'blur'}
+          ]
+        },
+        concludingReportRule: {
+          startTime: [
+            {required: true, type: 'date', message: '请选择开始时间', trigger: 'blur'}
+          ],
+          endTime: [
+            {required: true, type: 'date', message: '请选择截止时间', trigger: 'blur'}
+          ],
+          uploadAddress: [
+            {required: true, message: '请上传模板', trigger: 'blur'}
+          ]
+        },
+        uploadHeaders: {
+          Authorization: localStorage.getItem('token')
+        },
         data1: [],
         columns: [
           {
@@ -197,6 +309,92 @@
           this.$Message.error("请检查网络连接！")
           this.loading = false
         })
+      },
+      openInterimReport(index) {
+        this.modal3 = true
+        this.index = index
+      },
+      openConcludingReport(index) {
+        this.modal4 = true
+        this.index = index
+      },
+      uploadSuccess1(response) {
+        this.interimReport.uploadAddress = response.data
+        this.$Message.success("上传成功！");
+        console.log('uploadAddress:', this.interimReport.uploadAddress)
+      },
+      uploadSuccess2(response) {
+        this.concludingReport.uploadAddress = response.data
+        this.$Message.success("上传成功！");
+        console.log('uploadAddress:', this.concludingReport.uploadAddress)
+      },
+      ok1(index) {
+        this.$refs['interimReport'].validate((valid) => {
+          if (valid) {
+            axios({
+              url: apiRoot + '/admin/createReport',
+              method: 'post',
+              data: {
+                projectCategoryId: this.data1[index].projectCategoryId,
+                type: 1,
+                reportAddress: this.interimReport.uploadAddress,
+                startTime: this.interimReport.startTime,
+                deadline: this.interimReport.endTime
+              }
+            }).then((res) => {
+              if (res.data.code === 'SUCCESS') {
+                this.data1[index].interimReport.isReportActivated = true;
+                this.data1[index].interimReport.startTime = res.data.startTime;
+                this.data1[index].interimReport.deadline = res.data.endTime;
+                this.$Message.success('开通中期报告成功！');
+                this.$refs['interimReport'].resetFields()
+                this.modal3 = false;
+              } else {
+                this.$Message.error('开通失败！')
+              }
+            })
+          } else {
+            this.$Message.error('请将有关字段补充完整！');
+          }
+        })
+      },
+      cancel1() {
+        this.modal3 = false
+        this.$refs['interimReport'].resetFields()
+      },
+      ok2(index) {
+        this.$refs['concludingReport'].validate((valid) => {
+          if (valid) {
+            axios({
+              url: apiRoot + '/admin/createReport',
+              method: 'post',
+              data: {
+                projectCategoryId: this.data1[index].projectCategoryId,
+                type: 2,
+                reportAddress: this.concludingReport.uploadAddress,
+                startTime: this.concludingReport.startTime,
+                deadline: this.concludingReport.endTime
+              }
+            }).then((res) => {
+              if (res.data.code === 'SUCCESS') {
+                this.data1[index].concludingReport.isReportActivated = true;
+                this.data1[index].concludingReport.startTime = res.data.startTime;
+                this.data1[index].concludingReport.deadline = res.data.endTime;
+                this.$Message.success('开通中期报告成功！');
+                this.$refs['concludingReport'].resetFields()
+                this.modal4 = false;
+              } else {
+                this.$Message.error('开通失败！')
+              }
+            })
+          } else {
+            this.$Message.error('请将有关字段补充完整！');
+          }
+        })
+      },
+      cancel2() {
+        this.modal4 = false
+        this.$refs['concludingReport'].resetFields()
       }
     }
   }
